35635435da2e6ebfa725fa062f3dcf34d54f02fb
thang <thanghn2503@gamil.com>
2022-04-23 09:57:27 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,109 @@
+import requests
+import json
+import pymongo
+import time
+from win10toast import ToastNotifier
+from geopy.geocoders import Nominatim
+import geocoder
+from tkinter import *
+import threading
+
+win = Tk()
+win.title('Weather')
+
+g = geocoder.ip('me')
+myLocation = g.city     # lấy tên thành phố
+
+def theWeatherNow():
+    data = resultOfSearch(myLocation)
+    text = f'main: {data["main"]} \n'
+    # print(data['main']["humidity"])
+    # while True:
+    #     t = ToastNotifier()
+    #     t.show_toast(f"Weather of {myLocation} today:",text,duration = 5)
+    #     # print('ok')
+    #     break
+def show_weather_here():
+    show_weather = threading.Thread(target = theWeatherNow)
+    show_weather.start()
+
+def resultOfSearch(location):
+    x = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid=a7c7bb6f3e9b61aee6966b09d3e30214')
+    data = json.loads(x.text)
+    return data
+
+
+def weatherOfCity(city):
+    data = resultOfSearch(city)
+    temp = data['main']['temp']
+    humid = data['main']['humidity']
+    for widgets in frame4.winfo_children():
+        widgets.destroy()
+    lb1 = Label(frame4,text=f'Thành phố {city}', font="Time 12 bold")
+    lb2 = Label(frame4,text=f'Nhiệt độ {temp}', font="Time 12")
+    lb3 = Label(frame4,text=f'Độ ẩm {humid}', font="Time 12")
+    lb1.pack()
+    lb2.pack()
+    lb3.pack()
+    
+def closeSearchFunction():
+    frame3.pack_forget()
+    frame4.pack_forget()
+
+def searchFunction():
+    textInput = Entry(frame3, font='Times 15')
+    textInput.grid(row =4, column =0,pady= 20)
+    def thirdThread():
+        k = threading.Thread(target = weatherOfCity(textInput.get()))
+        k.start()
+    btn = Button(frame3, text="search" ,font='Times 12', command=thirdThread)
+    btn.grid(row =4, column = 1)
+    btn2 = Button(frame3, text="Cancel", font='Times 12', command=closeSearchFunction)
+    btn2.grid(row =4, column = 3)
+    frame3.pack()
+    frame4.pack()
+
+
+
+screenWidth = win.winfo_screenwidth()
+screenHeight = win.winfo_screenheight()
+consoleWidth = 500
+consoleHeight = 500
+win.geometry(f'{consoleWidth}x{consoleHeight}+%d+%d' %(screenWidth/2 - consoleWidth/2, screenHeight/2 - consoleHeight/2 - 80))
+frame1 = Frame(win)
+frame2 = Frame(win)
+frame3 = Frame(win)
+frame4 = Frame(win)
+lb1 = Label(frame1, text="Chọn chức năng", font="Times 14 bold")
+lb2 = Label(frame2, text="Xem thời tiết chỗ bạn", font="Times 14")
+lb3 = Label(frame2, text="Xem thời tiết tỉnh thành khác", font="Times 14")
+lb4 = Label(frame2, text="Tỉnh thành phố có thời tiết đẹp", font="Times 14")
+btn1 = Button(frame2, text="Chọn", command=show_weather_here)
+btn2 = Button(frame2, text="Nhập", command=searchFunction)
+btn3 = Button(frame2, text="Chọn")
+lb1.pack()
+lb2.grid(row=0, column=0,sticky=W)
+lb3.grid(row=1, column=0,sticky=W)
+lb4.grid(row=2, column=0,sticky=W)
+btn1.grid(row=0, column=1)
+btn2.grid(row=1, column=1)
+btn3.grid(row=2, column=1)
+frame1.pack()
+frame2.pack()
+frame3.pack()
+frame4.pack()
+
+# -------------------------------------------------------------------------------------------------------------------
+
+
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["AI"]
+# mycollection = mydb["cities"]
+
+
+
+
+
+show_weather_here()
+win.mainloop()
+
---
+++ test.py
@@ -0,0 +1,50 @@
+# import geocoder
+# g = geocoder.ip('me')
+# print(g.state)
+# print(g.json)
+
+# x = requests.get('https://api.openweathermap.org/data/2.5/weather?q=thành phố nam định&appid=a7c7bb6f3e9b61aee6966b09d3e30214')
+# k = json.loads(x.text)
+# print(k['weather'][0]['main'] + k['weather'][0]['description'])
+# x = requests.get('https://provinces.open-api.vn/api/?depth=3')
+# k = json.loads(x.text)
+# print(k[0]['name'])
+
+
+from tkinter import *
+win = Tk()
+win.title('Weather')
+# win.configure(width=400, height=200)
+screenWidth = win.winfo_screenwidth()
+screenHeight = win.winfo_screenheight()
+consoleWidth = 500
+consoleHeight = 500
+win.geometry(f'{consoleWidth}x{consoleHeight}+%d+%d' %(screenWidth/2 - consoleWidth/2, screenHeight/2 - consoleHeight/2 - 80))
+# lb1 = Label(win, text = 'hello')
+# lb1.pack()
+# def setText():
+#     lb1.config(text = "xin chao")
+#     lb1.pack()
+# win.after(1000,setText)
+
+def display():
+    print(textInput.get())
+def show(e):
+    print('hello')
+    
+textInput = Entry(win, font='Times 20 bold')
+textInput.pack()
+btn = Button(win,text='tim kiem', font='Times 10 bold', command=display)
+btn.pack()
+# win.resizable(width=False, height=False)
+# frame = Frame(win)
+# frame.pack()
+# def xuat():
+#     print('xin chao')
+# button = Button(frame, text='click me', command = xuat)
+# button.pack()
+
+# Label(win, text='xin chao').grid(row=0,column=0)
+# Label(win, text='hello').grid(row=0,column=1)
+
+win.mainloop()
